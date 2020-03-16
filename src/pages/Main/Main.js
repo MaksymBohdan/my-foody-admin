ad0f9a422d731449966bc6a8394c24ceb02b322c
MaksymBohdan <m.bohdan@globit.com>
2020-03-16 18:30:58 +0200

    feat/ant

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 
 const Main = () => {
-  return <div>MAIN PAGE</div>;
+  return (
+    <div>
+      MAIN PAGE <Button type="primary">Button</Button>
+    </div>
+  );
 };
 
 export default Main;
